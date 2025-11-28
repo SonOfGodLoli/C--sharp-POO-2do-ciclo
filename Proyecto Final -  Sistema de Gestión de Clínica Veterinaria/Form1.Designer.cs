988ec8c26e9998de055aa56a75b6847ef1af6b88
SonOfGodLoli <ricardocanuntongarcia@gmail.com>
2025-11-28 18:20:20 -0500

    dsddsd

--- Proyecto Final -  Sistema de Gestión de Clínica Veterinaria/Form1.Designer.cs
+++ Proyecto Final -  Sistema de Gestión de Clínica Veterinaria/Form1.Designer.cs
@@ -28,18 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(256, 346);
+            button1.Name = "button1";
+            button1.Size = new Size(261, 107);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(598, 346);
+            button2.Name = "button2";
+            button2.Size = new Size(261, 107);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(929, 346);
+            button3.Name = "button3";
+            button3.Size = new Size(261, 107);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(446, 555);
+            button4.Name = "button4";
+            button4.Size = new Size(261, 107);
+            button4.TabIndex = 4;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(798, 555);
+            button5.Name = "button5";
+            button5.Size = new Size(261, 107);
+            button5.TabIndex = 3;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1546, 949);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }

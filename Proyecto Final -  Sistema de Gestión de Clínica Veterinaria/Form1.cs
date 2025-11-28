a22c103d5b517b89762817fb39e3e93f4e191ab4
SonOfGodLoli <ricardocanuntongarcia@gmail.com>
2025-11-28 15:23:33 -0500

    cambios y detalles

---
+++ Proyecto Final -  Sistema de Gestión de Clínica Veterinaria/Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_Final____Sistema_de_Gestión_de_Clínica_Veterinaria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
